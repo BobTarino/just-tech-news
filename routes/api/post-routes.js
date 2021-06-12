284bfebe949075a9e351d3e1559dc2a92596bc95
Bobby T <bob.tarino@gmail.com>
2021-06-12 16:50:34 -0500

    added delete route for Post model and fixed order of posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,6 +6,7 @@ const { Post, User } = require('../../models');
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: ['id', 'post_url', 'title', 'created_at'],
+      order: [['created_at', 'DESC']], 
       // JOIN with User table using include property
       include: [
         {
@@ -85,6 +86,25 @@ router.put('/:id', (req, res) => {
           console.log(err);
           res.status(500).json(err);
         });
+});
+// delete a Post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 
