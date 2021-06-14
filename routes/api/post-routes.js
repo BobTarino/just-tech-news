542be64df26d0a966b78d0f1263d33ca2212853f
Bobby T <bob.tarino@gmail.com>
2021-06-13 19:44:09 -0500

    refactored code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -51,11 +51,20 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
+        // include the Comment model here:
         {
-          model: User,
-          attributes: ['username']
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
         }
-      ]
+    ]
     })
       .then(dbPostData => {
         if (!dbPostData) {
