f0a3c15948e572478cb627644825e2adfc16bf06
Bobby T <bob.tarino@gmail.com>
2021-06-13 19:27:31 -0500

    create api routes for Comment api

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,9 +2,23 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'comment_text',
+            'created_at',
+            // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
 
+        order: [['created_at', 'DESC']],
+    })
+      // remember to always include promise to capture reponse from db call
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
-
 router.post('/', (req, res) => {
     Comment.create({
         comment_text: req.body.comment_text,
@@ -18,8 +32,24 @@ router.post('/', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
 
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
