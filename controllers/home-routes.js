96b02b85109f7ef68ac1314b29436e121da87a36
Bobby T <bob.tarino@gmail.com>
2021-06-17 12:58:59 -0500

    create Homepage template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    // render homepage.handlebars template
-    res.render('homepage');
+    res.render('homepage', {
+      id: 1,
+      post_url: 'https://handlebarsjs.com/guide/',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      vote_count: 10,
+      comments: [{}, {}],
+      user: {
+        username: 'test_user'
+      }
+    });
 });
 
 module.exports = router;
