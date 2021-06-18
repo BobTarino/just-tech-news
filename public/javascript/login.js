cdb4141d32672e64377b9e9cec35d96f5ee98bf2
Bobby T <bob.tarino@gmail.com>
2021-06-18 16:43:51 -0500

    added login form handler event function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,6 @@
-// listening for submit event from form
-function signupFormHandler(event) {
+
+async function signupFormHandler(event) {
+
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
@@ -8,16 +9,46 @@ function signupFormHandler(event) {
   
     // condiional makes sure all fields have values before making POST request
     if (username && email && password) {
-      fetch('/api/users', {
+        const response = await fetch('/api/users', {
+          method: 'post',
+          body: JSON.stringify({
+            username,
+            email,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+            // check the response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
-          username,
           email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
-      }).then((response) => {console.log(response)})
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
 }
-
+  
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
