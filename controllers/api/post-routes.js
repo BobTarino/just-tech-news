5255224e755331435f0709366d45eddfe6ebfb78
Bobby T <bob.tarino@gmail.com>
2021-06-20 18:27:54 -0500

    added data and logic to dashboard

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,7 +85,7 @@ router.post('/', (req, res) => {
     Post.create({
       title: req.body.title,
       post_url: req.body.post_url,
-      user_id: req.body.user_id
+      user_id: req.session.user_id
       // Sequelize will add created_at and updated_at
     })
       .then(dbPostData => res.json(dbPostData))
