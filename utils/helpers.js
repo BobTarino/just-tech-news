3815d5b4d35f0245bf1f0b225f39d4b44d12fa0f
Bobby T <bob.tarino@gmail.com>
2021-06-20 15:10:35 -0500

    write and fullfil test for plural words

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,17 @@
+
+// method will return word with or without "s" depending on second arhument
 module.exports = {
     format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+            date
+          ).getFullYear()}`;
+    },
+    format_plural: (word, amount) => {
+      if (amount !== 1) {
+        return `${word}s`;
+      }
+  
+      return word;
     }
 }
+  
